b5428d438a4857ab8dc4c62280d62a7ffac2880c
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2025-06-28 19:46:35 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import Dashboard from "./pages/Dashboard"
+import {BrowserRouter , Route, Routes} from 'react-router-dom'
+import {Signup} from "./components/Signup"
+import {Login} from "./components/Login"
 
 
 function App() {
@@ -6,13 +9,18 @@ function App() {
   return (
     <>
 
-    
+
       <div class="absolute top-0 z-[-2] min-h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
         
-        <Dashboard />
-        
-        
-        </div>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
